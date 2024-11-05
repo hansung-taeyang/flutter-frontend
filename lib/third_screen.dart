aab45cba31cfeed4cf97aee652f091dc6ff97ef9
duddn0406 <64262606+duddn0406@users.noreply.github.com>
2024-11-05 14:27:08 +0900

    Update third_screen.dart

--- lib/third_screen.dart
+++ lib/third_screen.dart
@@ -5,6 +5,42 @@ class ThirdPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+          child: SingleChildScrollView(
+          padding: const EdgeInsets.fromLTRB(20.0, 150.0, 20.0, 20.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(padding: const EdgeInsets.all(5.0), // 원하는 만큼 패딩을 설정
+                  child: SizedBox(
+                  width: 500,
+                  height: 200,
+                    child: Container(
+                      color: Colors.white,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(Icons.person
+                          ),
+                          Column(
+
+                          )
+                        ],
+                      ),
+
+                    ),
+
+                  ),
+                )
+              ],
+            ),
+          )
+          )
+      )
+    );
   }
 }
