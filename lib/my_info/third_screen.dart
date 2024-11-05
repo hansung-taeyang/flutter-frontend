44d9c6bcc58268517eb8fe5392fabb53474f33df
duddn0406 <64262606+duddn0406@users.noreply.github.com>
2024-11-05 14:29:54 +0900

    Update third_screen.dart

--- lib/my_info/third_screen.dart
+++ lib/my_info/third_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:precapstone/authentication/login_screen.dart'; // LoginPage가 정의된 파일 import
 
 class ThirdPage extends StatelessWidget {
   const ThirdPage({super.key});
@@ -7,40 +8,159 @@ class ThirdPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: SingleChildScrollView(
-          padding: const EdgeInsets.fromLTRB(20.0, 150.0, 20.0, 20.0),
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0), // 위 여백을 약간만 남김
           child: Center(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Padding(padding: const EdgeInsets.all(5.0), // 원하는 만큼 패딩을 설정
-                  child: SizedBox(
-                  width: 500,
-                  height: 200,
-                    child: Container(
-                      color: Colors.white,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Icon(Icons.person
-                          ),
-                          Column(
-
-                          )
-                        ],
+                Container(
+                  padding: const EdgeInsets.only(left: 40.0, right: 20.0, top: 20.0, bottom: 20.0),
+                  width: 400,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
                       ),
-
-                    ),
-
+                    ],
                   ),
-                )
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        radius: 40,
+                        backgroundColor: Colors.pinkAccent.withOpacity(0.3),
+                        child: Icon(
+                          Icons.person,
+                          size: 50,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Center(
+                              child: Text(
+                                "User Name",
+                                style: TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black87,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 5),
+                            Center(
+                              child: Text(
+                                "user@example.com",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.black54,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            FractionallySizedBox(
+                              widthFactor: 0.67,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  // 정보 수정 버튼의 기능 추가
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.blueAccent,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                ),
+                                child: Text("정보 수정"),
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            FractionallySizedBox(
+                              widthFactor: 0.67,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const LoginPage(),
+                                    ),
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.redAccent,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                ),
+                                child: Text("로그아웃"),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20), // 박스 아래 여백
+                SizedBox(
+                  width: 400,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // 공지사항 버튼 기능 추가
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text(
+                      "공지사항",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10), // 버튼 간 간격
+                SizedBox(
+                  width: 400,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // 서비스 안내 버튼 기능 추가
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text(
+                      "서비스 안내",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
-          )
-          )
-      )
+          ),
+        ),
+      ),
     );
   }
 }
