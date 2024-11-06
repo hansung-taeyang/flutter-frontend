74ca9fd73bf309954a21d2bb916a10c35893da0e
sumi <lsmkoo03@naver.com>
2024-11-06 14:53:17 +0900

    화면 전환 순서 변경

--- lib/home/main_screen.dart
+++ lib/home/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:precapstone/send_message/check_image_screen.dart';
 import 'package:precapstone/send_message/create_image_screen.dart';
-import 'package:precapstone/send_message/first_screen.dart';
+import 'package:precapstone/send_message/write_message.dart';
 import '../message_history/second_screen.dart';
 import '../my_info/third_screen.dart';
 import '../const/colors.dart';
@@ -14,6 +15,8 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   int _selectedIndex = 0;
+  int _subIndex = 0; // 첫 번째 탭 내에서 페이지 구분
+  String imageUrl = '';
 
   void _onItemTapped(int index) {
     setState(() {
@@ -21,16 +24,77 @@ class _MainPageState extends State<MainPage> {
     });
   }
 
-  void _navigateToCreateImagePage() {
+  void _backToCreateImagePage() {
     setState(() {
-      _selectedIndex = 3; // 이미지 생성 페이지로 이동
+      _subIndex = 0; // 이미지 생성 페이지로 이동
     });
   }
 
-  void _navigateToSendMessagePage() {
+  void _goToWriteMessagePage() {
     setState(() {
-      _selectedIndex = 0; // 문자 보내기 페이지로 이동
+      _subIndex = 2; // 문자 작성 페이지로 이동
     });
+  }
+
+  void _goToCheckImagePage(String imageUrl) {
+    this.imageUrl = imageUrl;
+    setState(() {
+      _subIndex = 1; // 이미지 확인 페이지로 이동
+    });
+  }
+
+  void _backToCheckImagePage() {
+    setState(() {
+      _subIndex = 1; // 이미지 확인 페이지로 이동
+    });
+  }
+
+  Widget _getCurrentPage() {
+    if (_selectedIndex == 0) {
+      // 첫 번째 탭에서는 _subIndex에 따라 페이지 구분
+      switch (_subIndex) {
+        case 0:
+          return CreateImagePage(navigateToCheckImage: _goToCheckImagePage);
+        case 1:
+          return CheckImagePage(
+            imageUrl: imageUrl,
+            navigateToWriteMessage: _goToWriteMessagePage,
+            navigateToCrateImage: _backToCreateImagePage,
+          );
+        case 2:
+          return WriteMessagePage(
+            imageUrl: imageUrl,
+            navigateToCheckImage: _backToCheckImagePage,
+          );
+        default:
+          return CreateImagePage(navigateToCheckImage: _goToCheckImagePage);
+      }
+    } else if (_selectedIndex == 1) {
+      return const SecondPage();
+    } else {
+      return const ThirdPage();
+    }
+  }
+
+  String _getAppBarTitle() {
+    if (_selectedIndex == 0) {
+      // 첫 번째 탭에서 subIndex에 따라 타이틀 변경
+      switch (_subIndex) {
+        case 0:
+          return '이미지 생성';
+        case 1:
+          return '이미지 확인';
+        case 2:
+          return '문자 작성';
+        default:
+          return '';
+      }
+    } else if (_selectedIndex == 1) {
+      return '문자 기록 조회';
+    } else if (_selectedIndex == 2) {
+      return '내 정보';
+    }
+    return '';
   }
 
   @override
@@ -41,23 +105,13 @@ class _MainPageState extends State<MainPage> {
         title: Padding(
           padding: const EdgeInsets.only(top: 10.0), // 텍스트를 아래로 내림
           child: Text(
-            _getAppBarTitle(_selectedIndex),
+            _getAppBarTitle(),
             style: const TextStyle(
                 fontWeight: FontWeight.bold, fontSize: 24, color: whiteColor),
           ),
         ),
       ),
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: [
-          FirstPage(navigateToCrateImage: _navigateToCreateImagePage), // 콜백 전달
-          const SecondPage(),
-          const ThirdPage(),
-          CreateImagePage(
-            navigateToSendMessage: _navigateToSendMessagePage,
-          ), // 콜백 전달
-        ],
-      ),
+      body: _getCurrentPage(),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: backgroundColor,
         items: const <BottomNavigationBarItem>[
@@ -74,28 +128,12 @@ class _MainPageState extends State<MainPage> {
             label: '세 번째',
           ),
         ],
-        currentIndex: _selectedIndex == 3 ? 0 : _selectedIndex,
+        currentIndex: _selectedIndex,
         selectedItemColor: deepBlueColor,
         onTap: _onItemTapped,
         showSelectedLabels: false, // 선택된 항목의 라벨 숨김
         showUnselectedLabels: false, // 선택되지 않은 항목의 라벨 숨김
       ),
     );
-  }
-
-  String _getAppBarTitle(int index) {
-    switch (index) {
-      case 0:
-        return '문자 보내기';
-
-      case 1:
-        return '문자 기록 조회';
-      case 2:
-        return '내 정보';
-      case 3:
-        return '이미지 생성';
-      default:
-        return '';
-    }
   }
 }
