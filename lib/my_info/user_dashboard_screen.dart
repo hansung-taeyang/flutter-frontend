eb1b1ad8819b154917bb45e42b070e8a34930226
sumi <lsmkoo03@naver.com>
2024-11-17 01:22:52 +0900

    내 정보 페이지 UI 수정 (웹화면 고려)

--- lib/my_info/user_dashboard_screen.dart
+++ lib/my_info/user_dashboard_screen.dart
@@ -3,159 +3,172 @@ import 'package:precapstone/authentication/sign_in_logic.dart';
 import 'package:precapstone/authentication/sign_in_screen.dart';
 import 'package:precapstone/const/colors.dart';
 
-class ThirdPage extends StatelessWidget {
-  const ThirdPage({super.key});
+class UserDashboardPage extends StatelessWidget {
+  const UserDashboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final AuthService authService = AuthService(); // AuthService 인스턴스 생성
+    AuthService authService = AuthService();
 
-    return Scaffold(
-      backgroundColor: backgroundColor,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding:
-              const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0), // 위 여백을 약간만 남김
-          child: Center(
-            child: Column(
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(
-                      left: 30.0, right: 10.0, top: 20.0, bottom: 20.0),
-                  width: 400,
-                  decoration: BoxDecoration(
-                    color: whiteColor,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const CircleAvatar(
-                        radius: 50,
-                        backgroundColor: lightPinkColor,
-                        child: Icon(
-                          Icons.person,
-                          size: 50,
-                          color: normalPinkColor,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        var screenSize = MediaQuery.of(context).size;
+        var isWeb = screenSize.width > 600;
+
+        return Scaffold(
+          backgroundColor: backgroundColor,
+          body: SingleChildScrollView(
+            // 스크롤 가능한 구조로 변경 (웹 화면에서 bottom overflow 문제 방지)
+            child: Center(
+              child: Column(
+                children: [
+                  const SizedBox(height: 20),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: whiteColor,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    width: isWeb ? 500 : screenSize.width * 0.9,
+                    height: 160,
+                    padding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: lightPinkColor,
+                          ),
+                          child: const Icon(
+                            Icons.person,
+                            size: 50,
+                            color: normalPinkColor,
+                          ),
                         ),
-                      ),
-                      const SizedBox(width: 20),
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
+                        const SizedBox(width: 20),
+                        Column(
                           children: [
-                            const Center(
-                              child: Text(
-                                "user@example.com",
-                                style: TextStyle(
-                                  fontSize: 17,
-                                  color: blackColor,
-                                ),
+                            const Expanded(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Center(
+                                    child: Text(
+                                      "user@example.com",
+                                      style: TextStyle(
+                                        fontSize: 17,
+                                        color: blackColor,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(height: 5),
+                                ],
                               ),
                             ),
-                            const SizedBox(height: 20),
-                            FractionallySizedBox(
-                              widthFactor: 0.67,
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  // 정보 수정 버튼의 기능 추가
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: lightBlueColor,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(15),
-                                  ),
+                            ElevatedButton(
+                              onPressed: () {
+                                // 정보 수정 버튼의 기능 추가 예정
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: lightBlueColor,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15),
                                 ),
-                                child: const Text("정보수정",
-                                    style: TextStyle(color: deepBlueColor)),
                               ),
+                              child: const Text("정보수정",
+                                  style: TextStyle(color: deepBlueColor)),
                             ),
                             const SizedBox(height: 5),
-                            FractionallySizedBox(
-                              widthFactor: 0.67,
-                              child: ElevatedButton(
-                                onPressed: () async {
-                                  await authService.logout(); // 토큰 삭제
-                                  Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => LoginPage(),
-                                    ),
-                                  );
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: greyColor,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(15),
+                            ElevatedButton(
+                              onPressed: () async {
+                                await authService.logout(); // 토큰 삭제
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const LoginPage(),
                                   ),
+                                );
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: greyColor,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15),
                                 ),
-                                child: const Text(
-                                  "로그아웃",
-                                  style: TextStyle(color: whiteColor),
-                                ),
+                              ),
+                              child: const Text(
+                                "로그아웃",
+                                style: TextStyle(color: whiteColor),
                               ),
                             ),
                           ],
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20), // 박스 아래 여백
+                  Container(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          width: isWeb ? 500 : screenSize.width * 0.9,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // 공지사항 버튼 기능 추가 예정
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: normalPinkColor,
+                              padding: const EdgeInsets.symmetric(vertical: 15),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                            ),
+                            child: const Text(
+                              "공지사항",
+                              style: TextStyle(
+                                color: whiteColor,
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 20), // 박스 아래 여백
-                SizedBox(
-                  width: 400,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // 공지사항 버튼 기능 추가
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: normalPinkColor,
-                      padding: const EdgeInsets.symmetric(vertical: 15),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    ),
-                    child: const Text(
-                      "공지사항",
-                      style: TextStyle(
-                        color: whiteColor,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10), // 버튼 간 간격
-                SizedBox(
-                  width: 400,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // 서비스 안내 버튼 기능 추가
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: deepPinkColor,
-                      padding: const EdgeInsets.symmetric(vertical: 15),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    ),
-                    child: const Text(
-                      "서비스 안내",
-                      style: TextStyle(
-                        color: whiteColor,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                      ),
+                        const SizedBox(height: 10),
+                        SizedBox(
+                          width: isWeb ? 500 : screenSize.width * 0.9,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // 서비스 안내 버튼 기능 추가 예정
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: deepPinkColor,
+                              padding: const EdgeInsets.symmetric(vertical: 15),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                            ),
+                            child: const Text(
+                              "서비스 안내",
+                              style: TextStyle(
+                                color: whiteColor,
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
