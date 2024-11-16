cfa8d95f7d62fc6f0952351a87ea92a978a8aea7
sumi <lsmkoo03@naver.com>
2024-11-17 01:11:05 +0900

    이미지 확인 페이지 UI 수정

--- lib/send_message/check_image_screen.dart
+++ lib/send_message/check_image_screen.dart
@@ -28,99 +28,94 @@ class _CheckImagePageState extends State<CheckImagePage> {
 
         return Scaffold(
           backgroundColor: backgroundColor,
-          body: Center(
-            child: SingleChildScrollView(
-              padding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 20.0),
+          body: SingleChildScrollView(
+            child: Center(
               child: Column(
                 children: [
-                  const SizedBox(height: 10),
+                  const SizedBox(height: 20),
                   Container(
                     padding: const EdgeInsets.all(12),
                     decoration: BoxDecoration(
                       color: whiteColor,
                       borderRadius: BorderRadius.circular(15),
                     ),
-                    width: isWeb ? 500 : double.infinity,
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 100),
-                        Container(
-                          constraints: BoxConstraints(
-                            maxHeight: screenSize.height * 0.6,
-                          ),
-                          child: widget.imageUrl != null
-                              ? Image.network(
-                                  'http://$address:3000${widget.imageUrl!}',
-                                  fit: BoxFit.cover,
-                                  loadingBuilder: (BuildContext context,
-                                      Widget child,
-                                      ImageChunkEvent? loadingProgress) {
-                                    if (loadingProgress == null) return child;
-                                    return Center(
-                                      child: CircularProgressIndicator(
-                                        value: loadingProgress
-                                                    .expectedTotalBytes !=
-                                                null
-                                            ? loadingProgress
-                                                    .cumulativeBytesLoaded /
-                                                (loadingProgress
-                                                        .expectedTotalBytes ??
-                                                    1)
-                                            : null,
-                                      ),
-                                    );
-                                  },
-                                  errorBuilder: (context, error, stackTrace) =>
-                                      const Text(
-                                    '이미지를 불러올 수 없습니다.',
-                                    style: TextStyle(color: Colors.red),
-                                  ),
-                                )
-                              : const Text('이미지가 없습니다.'),
-                        ),
-                        const SizedBox(height: 110),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            ElevatedButton(
-                              onPressed: widget.navigateToCrateImage,
-                              style: ElevatedButton.styleFrom(
-                                foregroundColor: deepBlueColor,
-                                backgroundColor: normalBlueColor,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(50.0),
-                                ),
-                              ),
-                              child: const Text(
-                                '이전으로',
-                                style: TextStyle(
-                                  fontSize: 17.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(width: 12),
-                            ElevatedButton(
-                              onPressed: widget.navigateToWriteMessage,
-                              style: ElevatedButton.styleFrom(
-                                foregroundColor: whiteColor,
-                                backgroundColor: deepBlueColor,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(50.0),
-                                ),
-                              ),
-                              child: const Text(
-                                '문자 작성 하기',
-                                style: TextStyle(
-                                  fontSize: 17.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
+                    width: isWeb ? 500 : screenSize.width * 0.9,
+                    constraints: BoxConstraints(
+                      minHeight: isWeb
+                          ? screenSize.height * 0.5
+                          : screenSize.height * 0.7,
                     ),
+                    child: Center(
+                      child: widget.imageUrl != null
+                          ? Image.network(
+                              'http://$address:3000${widget.imageUrl!}',
+                              fit: BoxFit.cover,
+                              loadingBuilder: (BuildContext context,
+                                  Widget child,
+                                  ImageChunkEvent? loadingProgress) {
+                                if (loadingProgress == null) return child;
+                                return Center(
+                                  child: CircularProgressIndicator(
+                                    value: loadingProgress.expectedTotalBytes !=
+                                            null
+                                        ? loadingProgress
+                                                .cumulativeBytesLoaded /
+                                            (loadingProgress
+                                                    .expectedTotalBytes ??
+                                                1)
+                                        : null,
+                                  ),
+                                );
+                              },
+                              errorBuilder: (context, error, stackTrace) =>
+                                  const Text(
+                                '이미지를 불러올 수 없습니다.',
+                                style: TextStyle(color: Colors.red),
+                              ),
+                            )
+                          : const Text('이미지가 없습니다.'),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: widget.navigateToCrateImage,
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: deepBlueColor,
+                          backgroundColor: normalBlueColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50.0),
+                          ),
+                        ),
+                        child: const Text(
+                          '이전으로',
+                          style: TextStyle(
+                            fontSize: 17.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 12),
+                      ElevatedButton(
+                        onPressed: widget.navigateToWriteMessage,
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: whiteColor,
+                          backgroundColor: deepBlueColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50.0),
+                          ),
+                        ),
+                        child: const Text(
+                          '문자 작성 하기',
+                          style: TextStyle(
+                            fontSize: 17.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
